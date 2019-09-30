577278d1259cf40019ca91ba0cc3bd813fa6d229
Desi Desario <desireedesario@gmail.com>
2019-09-30 14:30:29 -0700

    took out unused models

--- beachDay/potluck/models.py
+++ beachDay/potluck/models.py
@@ -11,6 +11,7 @@ from django.forms import ModelForm, Textarea, CharField
 class PotluckItem(models.Model):
 	potluckItem_text = models.CharField(max_length=200, default="I.E. Canopy or 2x Dog Collar(s)", blank=True)
 	user_name = models.CharField(max_length=20, default="")
+	instagram_account = models.CharField(max_length=20, default="")
 	dog_name = models.CharField(max_length=20, default="", blank=True)
 	raffle_participation = models.BooleanField(default=False)
 	pub_date = models.DateTimeField(auto_now_add=True, blank=True)
@@ -37,7 +38,7 @@ class User(models.Model):
 class PotluckItemForm(forms.ModelForm):
 	class Meta:
 		model = PotluckItem
-		fields = ['user_name', 'dog_name', 'raffle_participation', 'potluckItem_text', 'additional_message']
+		fields = ['user_name', 'instagram_account' 'dog_name', 'raffle_participation', 'potluckItem_text', 'additional_message']
 		widgets = {
 			'additional_message' : Textarea,
 			'potluckItem_text' : Textarea,
@@ -45,6 +46,7 @@ class PotluckItemForm(forms.ModelForm):
 		labels = {
 			'user_name' : 'Name of Person/People Attending',
 			'dog_name' : "Name(s) of the Dog(s) Coming (not required)",
+			'instagram_account': "Instagram Account? (not required)",
 			'additional_message': "Additional Message (not required), i.e further detail about what you are bringing",
 			'potluckItem_text' : 'Potluck Item(s) you are Bringing (or enter your raffle items :D, but not required)',
 			'raffle_participation': "Will you be bringing anything for the raffle?"
@@ -55,30 +57,3 @@ class UserForm(forms.ModelForm):
 	class Meta:
 		model = User
 		fields = ['user_name']
-
-		############################
-		############################
-		############################
-
-# Create your models here.
-class Question(models.Model):
-	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField(auto_now_add=True, blank=True)
-	def __str__(self):
-		return self.question_text
-
-	def was_published_recently(self):
-		now = timezone.now()
-		return now - datetime.timedelta(days=1) <= self.pub_date <= now
-	was_published_recently.admin_order_field = 'pub_date'
-	was_published_recently.boolean = True
-	was_published_recently.short_description = 'Published recently?'
-
-
-class Choice(models.Model):
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)
-
-	def __str__(self):
-		return self.choice_text
